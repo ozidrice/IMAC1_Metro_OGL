dbf66af7c8608cea557640769071be584588b463
mariecruveillier <mariecruveillier@gmail.com>
2018-04-04 16:11:58 +0200

    changement ergonomie

---
+++ include/element.h
@@ -0,0 +1,21 @@
+#ifndef _MONDE_
+#define _MONDE_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Element {
+	int pv;
+	float posx, posy, taille;
+	char* texture;
+}Element;
+
+typedef struct ennemi {
+	Element e;
+	/* ... */
+}Ennemi;
+
+/* Initialise joueur/ennemis/obstacle */
+Element* initElement(int pv, float x, float y, float taille, char * texture);
+
+#endif
--- src/jeu.c
+++ src/jeu.c
@@ -4,6 +4,7 @@
 #include <SDL/SDL_image.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include "monde.h"
 
 static char *WINDOW_TITLE = "Jeu -- OpenGL";
 static unsigned int WINDOW_WIDTH = 800;
